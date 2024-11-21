abe5411c624da17efc11dd3d807e0f3c47db333e
tienvuiet <tienxinhzai241@GMAIL.COM>
2024-11-21 20:40:36 +0700

    basic2

---
+++ basic2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int n;
+	printf("nhap vao so nguyen n: ");
+	scanf("%d",&n);
+	if(n%2==0){
+		printf("day la so chan\n"); 
+	} else{
+		printf("day la so le ") ;
+	} 
+	return 0; 
+} 
